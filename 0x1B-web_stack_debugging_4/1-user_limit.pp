c6beba4f0533f3f52b9482d901625f6905982024
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-12-21 05:32:15 -0800

    a

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,24 +1,11 @@
-# creating holberton user
-user { 'holberton':
-  ensure     => 'present',
-  managehome => true,
+# Increase hard file limit for user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-# setting up a password for the "holberton" user
-user { 'holberton':
-  password => '$6$random_salt$hashed_password',
-}
-
-# Allowing SSH access for the "holberton" user
-ssh_authorized_key { 'holberton_ssh_key':
-  user  => 'holberton',
-  type  => 'ssh-rsa',
-  key   => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTk8M8LgFndnAoB++2PnC+T1b8RGTPuQi7b4Kh5Gyg70APTaTpWHaRBKEsZni8gN0u9A1yBkkIR3/twZ0YLOiDYLIl9sbAlxIr9v63liEX6d2ue7DCrwvl8xHN1Fi74GUzxtPta1peAHEq42+2Ibh1z41RhpK8AepMUFPNd/aCL6d83EchHvtFRWsRZpJpV2PRNKIBcfb628SuWaK85wlYfL2PHNIxHeXqaWBE8LSV7yvGfq2uNWMvPvGwF2hGEaoX/W4u2Ot/Lzaymr4mlR0RpZAxuv/QOLiWpg/0QPzVghSiINh31AYc3rtYutTCjwVVmMxRx4jcG9QonOQTpADgNT7axXCEM9gdDhlTcjoIcakWCaDU8ZUuMBXNPxIFioEXFz1/UdTokpCxNC7nt78k7BUwS3fkUNoEGoipeBhDsUzmL+FaSAMOwnzCGW/SipD4qtJcnM2sXXGnelZti6pXitsrMtdR8FT16Hm3FmbSLkWxTAlSlY+RpexCs2KYo30= root@5c47e3755b6d',
-}
-
-# changing permissions for a specific file
-file { '/usr/bin':
-  owner => 'holberton',
-  group => 'holberton',
-  mode  => '0644',
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
